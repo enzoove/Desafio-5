cdf4523ba447b25b94a0410c867a1e7ba8f8f0f9
enzoove <83042412+enzoove@users.noreply.github.com>
2022-11-02 03:42:10 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const server = require( './services/server' );
+
+const PORT = process.env.PORT || 8080;
+
+server.listen( PORT, ()=>{
+    console.log( `Servidor escuchando en puerto ${PORT}` );
+} )
